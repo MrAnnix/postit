09c1e40f5b70e5590d8606a5fbc0731e2f54fba5
Raúl San Martín Aniceto <9783500+MrAnnix@users.noreply.github.com>
2020-04-06 14:03:28 +0200

    Added password handling methods

--- src/main/java/es/raulsanmartin/postit/model/User.java
+++ src/main/java/es/raulsanmartin/postit/model/User.java
@@ -1,13 +1,28 @@
 package es.raulsanmartin.postit.model;
 
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
+import java.security.SecureRandom;
+
 public class User {
-    private String id;
+    private String id;    //Nickname w/o @
 
-    private String name;
+    private String name;  //User full name
 
-    private String email;
+    private String email; //User email
 
-    private String pswd;
+    private byte[] pswd;  //User hashed password
+
+    private byte[] salt;  //User salt to hash password with PBKDF2 algorithm, so pswd=hash(salt+realPassword)
+
+    private String photo; //User profle photo
+
+    private String header;//User profile header photo
 
     public String getId() {
         return id;
@@ -31,6 +46,49 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    //PASSWORD HANDLING
+    public Boolean checkPassword(String password) {
+        try {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+            PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 10000, 512);
+            SecretKey key = skf.generateSecret(spec);
+            return Arrays.equals(key.getEncoded(), pswd);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void setPassword(String password) {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[8];
+        random.nextBytes(salt);
+        try {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+            PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 10000, 512);
+            SecretKey key = skf.generateSecret(spec);
+            this.salt = salt;
+            this.pswd = key.getEncoded();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
     }
 
     public String getNick() {
